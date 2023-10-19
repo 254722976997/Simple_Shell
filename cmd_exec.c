fc9ec39eea912f76f7bc82b11e096b6ad5306f52
Helen Ndunge Kilolo <140470224+helenkilolo@users.noreply.github.com>
2023-10-20 00:57:27 +0300

    Update cmd_exec.c

--- cmd_exec.c
+++ cmd_exec.c
@@ -1,8 +1,8 @@
 #include "shell.h"
-#include "main.h"
 
 /**
  * is_cdir - checks ":" if is in the current directory.
+ * helen&martin
  * @path: type char pointer char.
  * @i: type int pointer of index.
  * Return: 1 if the path is searchable in the cd, 0 otherwise.
