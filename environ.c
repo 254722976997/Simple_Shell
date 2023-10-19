7368b28c6329260ba6e9927c8e7af41ed08bdf50
Helen Ndunge Kilolo <140470224+helenkilolo@users.noreply.github.com>
2023-10-20 00:38:23 +0300

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,105 +1,42 @@
 #include "shell.h"
 
 /**
- * print_current_environment - Prints the current environment
- * @info: Structure containing potential arguments
- *
- * This function prints the contents of the current environment.
- *
- * Return: Always 0.
+ * aux_help - Help information for the builtin help.
+ * helen&martin
+ * Return: no return
  */
-int print_current_environment(info_t *info)
+void aux_help(void)
 {
-	print_list_str(info->env);
-	return (0);
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
 }
-
 /**
- * get_environment_value - Gets the value of an environment variable.
- * @info: Structure containing potential arguments.
- * @name: Environment variable name.
- *
- * Functn retrieves the val of a specific envnt var based on its name.
- *
- * Return: The value of the environment variable or NULL if not found.
+ * aux_help_alias - Help information for the builtin alias.
+ * Return: no return
  */
-char *get_environment_value(info_t *info, const char *name)
+void aux_help_alias(void)
 {
-	list_t *current_node = info->env;
-	char *value;
+	char *help = "alias: alias [-p] [name[=value]...]\n";
 
-	while (current_node)
-	{
-		value = starts_with(current_node->str, name);
-		if (value && *value)
-			return (value);
-		current_node = current_node->next;
-	}
-	return (NULL);
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
 }
-
 /**
- * set_or_update_environment - Inits a new envnt variable or modify.
- * @info: Structure containing potential arguments.
- *
- * Functn either inits a new envnt var or modifies an existing one.
- *
- * Return: 0 on success, 1 on failure.
+ * aux_help_cd - Help information for the builtin alias.
+ * Return: no return
  */
-int set_or_update_environment(info_t *info)
+void aux_help_cd(void)
 {
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguments\n");
-	return (1);
-	}
+	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
 
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-
-	return (1);
-}
-
-/**
- * remove_environment_variable - Removes an environment variable.
- * @info: Structure containing potential arguments.
- *
- * This function removes an environment variable.
- *
- * Return: 0 on success, 1 on failure.
- */
-int remove_environment_variable(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{_eputs("Too few arguments.\n");
-		return (1);
-	}
-
-	for (i = 1; i < info->argc; i++)
-	_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_environment_list - Populates the environment linked list.
- * @info: Structure containing potential arguments.
- *
- * Function populates the environment linked list with environment var.
- *
- * Return: Always 0.
- */
-int populate_environment_list(info_t *info)
-{
-	list_t *current_node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-	add_node_end(&current_node, environ[i], i);
-
-	info->env = current_node;
-	return (0);
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
 }
 
